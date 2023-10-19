87ec56e662cc69ab7a4a7e95f4b61ac64c0b26b1
gmarokov <georgi.marokov@gmail.com>
2023-10-19 23:23:55 +0300

    docs: another post for ansible windows

--- src/components/partials/prism-syntax-highlight.js
+++ src/components/partials/prism-syntax-highlight.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+// import themeLight from 'prism-react-renderer/themes/github';
+// import themeDark from 'prism-react-renderer/themes/dracula';
+// import { isDarkTheme } from '../../utils/theme-checker';
 
 const PrismSyntaxHighlight = ({ children, className }) => {
+  // const theme = isDarkTheme() ? themeDark : themeLight;
   const language = className.replace(/language-/gm, '');
-
   return (
     <Highlight {...defaultProps} code={children} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
