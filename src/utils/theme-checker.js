60ee73f0a6a3a1fcdf327368030bbe53cb703b54
gmarokov <georgi.marokov@gmail.com>
2023-10-20 08:51:06 +0300

    docs: hangfire blog

--- src/utils/theme-checker.js
+++ src/utils/theme-checker.js
@@ -1,10 +1,10 @@
 export const isDarkTheme = () => {
-    if (
-        localStorage.getItem('color-theme') === 'light' ||
-        (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)
-      ) {
-        return false;
-      } else {  
-        return true
-      }
-  };
+  if (
+    localStorage.getItem('color-theme') === 'light' ||
+    (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+};
